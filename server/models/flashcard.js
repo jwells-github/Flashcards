47de80889b066030251224552b78b7be5cc6c5d2
Jake <jakewellsd@gmail.com>
2021-09-23 18:39:41 +0100

    add owner field to flashcard

--- server/models/flashcard.js
+++ server/models/flashcard.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const FlashcardSchema = new Schema({
     front_text: {type:String, required: true, minlength: 1},
     back_text: {type:String, default: ""}, 
-    date_created: {type: Date, default: Date.now}
+    date_created: {type: Date, default: Date.now},
+    owner: {type:Schema.Types.ObjectId, ref: 'User'}
 })
 
 module.exports = mongoose.model('Flashcard', FlashcardSchema);
