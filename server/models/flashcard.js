4105643bb1a8325c612e7ef0d168afe08dabe888
Jake <jakewellsd@gmail.com>
2021-09-24 17:42:03 +0100

    add cardDeck field

--- server/models/flashcard.js
+++ server/models/flashcard.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const FlashcardSchema = new Schema({
-    front_text: {type:String, required: true, minlength: 1},
-    back_text: {type:String, default: ""}, 
-    date_created: {type: Date, default: Date.now},
-    owner: {type:Schema.Types.ObjectId, ref: 'User'}
+    cardFront: {type:String, required: true, minlength: 1},
+    cardBack: {type:String, default: ""}, 
+    cardDeck: {type:String, require: true, minlength: 1},
+    dateCreated: {type: Date, default: Date.now},
+    owner: {type:Schema.Types.ObjectId, ref: 'User', required: true},
+
 })
 
 module.exports = mongoose.model('Flashcard', FlashcardSchema);
