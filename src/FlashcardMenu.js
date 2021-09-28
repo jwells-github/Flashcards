82b1e210ef0f4d2b21335d7d06fb2904ebf69c36
Jake <jakewellsd@gmail.com>
2021-09-28 21:31:34 +0100

    move autocomplete Input suggestion to its own generic componenet

--- src/FlashcardMenu.js
+++ src/FlashcardMenu.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import InputSuggestion from './InputSuggestion';
 class FlashcardMenu extends Component {
     constructor(props){
         super(props);
@@ -13,9 +14,7 @@ class FlashcardMenu extends Component {
         };
         this.createFlashcard = this.createFlashcard.bind(this);
         this.handleChange = this.handleChange.bind(this);
-        this.updateSuggestions = this.updateSuggestions.bind(this);
-        this.useSuggestion = this.useSuggestion.bind(this);
-        this.handleSuggestionInput = this.handleSuggestionInput.bind(this);
+        this.updateCardDeck = this.updateCardDeck.bind(this);
     }
 
     componentDidMount(){
@@ -34,16 +33,6 @@ class FlashcardMenu extends Component {
         
     }
 
-    updateSuggestions(event){
-        let cardDeck = event.target.value;
-        let regex = cardDeck.length > 0 ? cardDeck+'.' : "";
-
-        this.setState({ 
-            [event.target.name]: event.target.value,
-            selectedSuggestion: 0,
-            filteredDecks : this.state.decks.filter(deck => deck.match(new RegExp(regex)))
-         });
-    }
 
     handleChange(event) {
         this.setState({ [event.target.name]: event.target.value });
@@ -74,49 +63,10 @@ class FlashcardMenu extends Component {
           body: JSON.stringify({cardFront: this.state.cardFront, cardBack: this.state.cardBack, cardDeck: this.state.cardDeck}) 
         }).then(response => response.json()).then(response => this.addFlashcard(response));
     }
-
-    useSuggestion(suggestion){
-        this.setState({
-            cardDeck:suggestion,
-            selectedSuggestion: 0,
-            filteredDecks: []
-        })
-    }
-
-    handleSuggestionInput(event){
-        event.preventDefault();
-        let downKey = event.key === "Down" || event.key === "ArrowDown"
-        let upKey = event.key === "Up" || event.key === "ArrowUp"
-        let enterKey = event.key === "Enter";
-        if(downKey){
-            if(this.state.filteredDecks.length <= this.state.selectedSuggestion){
-                return;
-            }
-            this.setState(prevState =>({
-                selectedSuggestion: prevState.selectedSuggestion +1
-            }));
-        }
-        if(upKey){
-            if(this.state.selectedSuggestion <= 0){
-                return;
-            }
-            this.setState(prevState =>({
-                selectedSuggestion: prevState.selectedSuggestion -1
-            }));
-        }
-        if(enterKey){
-            if(this.state.selectedSuggestion === 0 ){
-                return;
-            }
-            console.log(this.state.filteredDecks)
-            console.log(this.state.selectedSuggestion)
-            this.useSuggestion(this.state.filteredDecks[this.state.selectedSuggestion -1] )
-        }
+    updateCardDeck(value){
+        this.setState({cardDeck:value})
     }
     render(){
-        console.log(this.state.cardDeck)
-        console.log(this.state.selectedSuggestion)
-        console.log(this.state.filteredDecks);
           return(
             <div className="entranceForm">
                 <h2>Hi</h2>
@@ -129,19 +79,11 @@ class FlashcardMenu extends Component {
                         <label htmlFor="cardBack">Back Text:</label>
                         <input name="cardBack" type="text" value={this.state.cardBack} onChange={this.handleChange}/>
                     </div>
-                    <div className="formField">
-                        <label htmlFor="cardDeck">Deck:</label>
-                        <input onKeyUp={this.handleSuggestionInput} name="cardDeck" type="text" value={this.state.cardDeck} onChange={this.updateSuggestions} autoComplete="off"/>
-                        <div className="formSuggestion">
-                            {this.state.filteredDecks.map((deck, index) => 
-                                <span
-                                    key={index}
-                                    id={this.state.selectedSuggestion === index + 1 ? "red" : ""}  
-                                    value={deck} 
-                                    onClick={() => this.useSuggestion(deck)}>{deck}
-                                </span>)}
-                        </div>
-                    </div>                    
+                    <InputSuggestion
+                        fieldName = {"Deck"}
+                        dataArray ={this.state.decks}
+                        returnValue = {this.updateCardDeck}
+                    />                 
                     <button type="submit">Create Card</button>
                 </form>
                 {this.state.decks}
---
+++ src/InputSuggestion.js
@@ -0,0 +1,82 @@
+import React, {Component} from 'react'
+
+
+class InputSuggestion extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            filteredData: [],
+            InputValue: '',
+            selectedSuggestion: 0,
+        };
+        this.updateSuggestions = this.updateSuggestions.bind(this);
+        this.useSuggestion = this.useSuggestion.bind(this);
+        this.handleSuggestionInput = this.handleSuggestionInput.bind(this);
+    }
+
+    handleSuggestionInput(event){
+        event.preventDefault();
+        let downKey = event.key === "Down" || event.key === "ArrowDown"
+        let upKey = event.key === "Up" || event.key === "ArrowUp"
+        let enterKey = event.key === "Enter";
+        if(downKey){
+            if(this.state.filteredData.length <= this.state.selectedSuggestion){
+                return;
+            }
+            this.setState(prevState =>({
+                selectedSuggestion: prevState.selectedSuggestion +1
+            }));
+        }
+        if(upKey){
+            if(this.state.selectedSuggestion <= 0){
+                return;
+            }
+            this.setState(prevState =>({
+                selectedSuggestion: prevState.selectedSuggestion -1
+            }));
+        }
+        if(enterKey){
+            if(this.state.selectedSuggestion === 0 ){
+                return;
+            }
+            this.useSuggestion(this.state.filteredData[this.state.selectedSuggestion -1] )
+        }
+    }
+
+    updateSuggestions(event){
+        this.setState({ 
+            InputValue: event.target.value,
+            selectedSuggestion: 0,
+            filteredData : this.props.dataArray.filter(data => data.match(new RegExp(event.target.value)))
+         });
+    }
+
+    useSuggestion(suggestion){
+        this.setState({
+            InputValue:suggestion,
+            selectedSuggestion: 0,
+            filteredData: []
+        })
+        this.props.returnValue(suggestion)
+    }
+
+      render(){
+          return(
+            <div className="formField">
+                <label htmlFor={this.props.fieldName}>{this.props.fieldName}:</label>
+                <input onKeyUp={this.handleSuggestionInput} name={this.props.fieldName} type="text" value={this.state.InputValue} onChange={this.updateSuggestions} autoComplete="off"/>
+                <div className="formSuggestion">
+                    {this.state.filteredData.map((item, index) => 
+                        <span
+                            key={index}
+                            id={this.state.selectedSuggestion === index + 1 ? "InputSuggestion-Selected" : ""}  
+                            value={item} 
+                            onClick={() => this.useSuggestion(item)}>{item}
+                        </span>)}
+                </div>
+            </div>  
+          )
+      }
+}
+
+export default InputSuggestion;
