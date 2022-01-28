ad2c93944261e5f6f7b0da690ba3ca0695fd53a6
Jake <jakewellsd@gmail.com>
2022-01-28 15:35:25 +0000

    add tests for InputSuggestion.js

--- src/Tests/InputSuggestion.test.js
+++ src/Tests/InputSuggestion.test.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import InputSuggestion from '../InputSuggestion';
@@ -84,7 +85,8 @@ test('the suggestions field is displayed when the textbox is focused', () =>{
     userEvent.click(screen.getByRole('textbox'));
     expect(screen.getByTestId('suggestionsContainer')).not.toHaveClass('hide');
 });
-test('the suggestions field is HIDDEN when the textbox is blurred', () =>{
+
+test('the suggestions field is hidden when the textbox is blurred', () =>{
     let testInputValue = "";
     render(
         <InputSuggestion 
@@ -97,3 +99,81 @@ test('the suggestions field is HIDDEN when the textbox is blurred', () =>{
     userEvent.click(screen.getByTestId('suggestionsContainer'));
     setTimeout(() => expect(screen.getByTestId('suggestionsContainer')).toHaveClass('hide'), InputSuggestion.suggestionDisplayTimeout)
 });
+
+test('pressing the down arrow in the suggestion field will highlight the suggestion below',() =>{
+    let testInputValue = "";
+    render(
+        <InputSuggestion 
+            inputValue={testInputValue}
+            updateInputValue={(value) => testInputValue=value}
+            dataArray={testDataArray}
+        />
+    );
+    userEvent.type(screen.getByRole('textbox'), 'F');
+    userEvent.type(screen.getByRole('textbox'), '{arrowdown}');
+    expect(screen.getByText('Fish Facts').id).toEqual('InputSuggestion-Selected');
+    expect(screen.getByText('Frog Facts').id).not.toEqual('InputSuggestion-Selected');
+    userEvent.type(screen.getByRole('textbox'), '{arrowdown}');
+    expect(screen.getByText('Frog Facts').id).toEqual('InputSuggestion-Selected');
+    expect(screen.getByText('Fish Facts').id).not.toEqual('InputSuggestion-Selected');
+});
+
+test('pressing the up arrow in the suggestion field will highlight the suggestion above',() =>{
+    let testInputValue = "";
+    render(
+        <InputSuggestion 
+            inputValue={testInputValue}
+            updateInputValue={(value) => testInputValue=value}
+            dataArray={testDataArray}
+        />
+    );
+    userEvent.type(screen.getByRole('textbox'), 'F');
+    userEvent.type(screen.getByRole('textbox'), '{arrowdown}');
+    userEvent.type(screen.getByRole('textbox'), '{arrowdown}');
+    userEvent.type(screen.getByRole('textbox'), '{arrowup}'); 
+    expect(screen.getByText('Fish Facts').id).toEqual('InputSuggestion-Selected');
+    expect(screen.getByText('Frog Facts').id).not.toEqual('InputSuggestion-Selected');
+});
+
+test('pressing enter on a selected suggestion will place its value in the form field',() =>{
+    let testInputValue = "";
+    const {rerender} = render(
+            <InputSuggestion 
+                InputValue={testInputValue}
+                updateInputValue={(value) => testInputValue=value}
+                dataArray={testDataArray}
+            />
+        );
+    userEvent.type(screen.getByRole('textbox'), 'F');
+    userEvent.keyboard('{arrowdown}')
+    userEvent.keyboard('[Enter]')
+    rerender(
+        <InputSuggestion 
+            InputValue={testInputValue}
+            updateInputValue={(value) => testInputValue=value}
+            dataArray={testDataArray}
+        />
+    )
+    expect((screen.getByRole('textbox').value)).toEqual('Fish Facts');    
+});
+
+test('clicking on a suggestions will place its value in the form field', () =>{
+    let testInputValue = "";
+    const {rerender} = render(
+            <InputSuggestion 
+                InputValue={testInputValue}
+                updateInputValue={(value) => testInputValue=value}
+                dataArray={testDataArray}
+            />
+        );
+    userEvent.type(screen.getByRole('textbox'), 'F');
+    userEvent.click(screen.getByText('Frog Facts'))
+    rerender(
+        <InputSuggestion 
+            InputValue={testInputValue}
+            updateInputValue={(value) => testInputValue=value}
+            dataArray={testDataArray}
+        />
+    )
+    expect((screen.getByRole('textbox').value)).toEqual('Frog Facts');    
+})
